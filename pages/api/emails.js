22572f7d7f685b8497fa682773e9204af52daf61
Chad Stark <chad.d.stark@hotmail.com>
2022-04-10 18:44:13 -0700

    added email format check to the api, responds accordingly

--- pages/api/emails.js
+++ pages/api/emails.js
@@ -8,9 +8,15 @@ export default async function handler(req, res) {
     first_name: req.body.first_name,
     email: req.body.email,
   });
-  await emailUser.save((err, user) => {
-    if (err) console.log(err);
-    console.log(user);
-  });
-  res.json({ example: "hello" });
+  if (emailUser.email.match(/^\S+@\S+\.\S+$/)) {
+    await emailUser.save((err, user) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.json({ res: "Successful signup", user: user });
+      }
+    });
+  } else {
+    res.json({ res: "Email format is not correct" });
+  }
 }
